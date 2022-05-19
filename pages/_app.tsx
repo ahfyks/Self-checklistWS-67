f1e4482221b96793f203e9ba5e46fed2abd76c9b
Fha D <43128683+ahfyks@users.noreply.github.com>
2022-05-19 09:37:47 +0700

    update files as baclup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import { AppProps } from "next/app";
 import Head from "next/head";
-import { MantineProvider } from "@mantine/core";
+import { Global, MantineProvider } from "@mantine/core";
+
 
 export default function App(props: AppProps) {
   const { Component, pageProps } = props;
@@ -8,19 +9,32 @@ export default function App(props: AppProps) {
   return (
     <>
       <Head>
-        <title>Mantine Radarchart</title>
+      <link rel="preconnect" href="https://fonts.googleapis.com"/>
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='true' />
+      <link href="https://fonts.googleapis.com/css2?family=Kanit:wght@300&display=swap" rel="stylesheet"/>
+        <title>Spider Chart หลักเกณฑ์การประเมินโครงการฯ</title>
         <meta
           name='viewport'
           content='minimum-scale=1, initial-scale=1, width=device-width'
         />
       </Head>
-
+      {/* <Global
+      styles={[
+        {
+          '@font-face': {
+            fontFamily: 'Kanit',
+            src: `url('https://fonts.googleapis.com/css2?family=Kanit:wght@300&display=swap')`,
+            fontStyle: 'normal',
+          },
+        },
+      ]}
+      /> */}
       <MantineProvider
         withGlobalStyles
         withNormalizeCSS
         theme={{
           /** Put your mantine theme override here */
-          fontFamily: 'Sarabun',
+          fontFamily: 'Kanit',
           colorScheme: "light",
           breakpoints: {
             xs: 500,
@@ -28,10 +42,10 @@ export default function App(props: AppProps) {
             md: 1000,
             lg: 1200,
             xl: 1400,
-          },
+          },         
         }}>
         <Component {...pageProps} />
       </MantineProvider>
-    </>
+    </> 
   );
-}
+};
